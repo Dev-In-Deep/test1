1dceddf7241d52e260a947684599bb34e2e533b3
Dmitry Safronov <aa502@yandex.ru>
2024-11-09 19:56:14 +0700

    build: добавлен laravel-ide-helper

--- app/Models/Guest.php
+++ app/Models/Guest.php
@@ -4,6 +4,30 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * 
+ *
+ * @property string $uuid
+ * @property string $first_name
+ * @property string $last_name
+ * @property string $phone
+ * @property string $email
+ * @property string $country
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Guest newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Guest newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Guest query()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Guest whereCountry($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Guest whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Guest whereEmail($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Guest whereFirstName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Guest whereLastName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Guest wherePhone($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Guest whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Guest whereUuid($value)
+ * @mixin \Eloquent
+ */
 class Guest extends Model
 {
     //
